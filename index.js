66f6d1b195d13f2a11dff68711300006121d6139
qamar serhan <serhanqamar@gmail.com>
2024-12-23 00:22:56 +0200

    dn step 3

--- index.js
+++ index.js
@@ -8,5 +8,22 @@ res.send ('ok') //send data back to the user
 })
 // make the application run 
 app .listen(3000) // our server is liseting on port 3000 for bunch of different requests 
-
 // now we have an app running on port 3000
+
+//rout for test 
+app.get ('/test', (req, res) => {
+ res. status(200) .send('ok')
+
+})
+
+//route for time 
+app.get('/time', (req, res)=>{
+    const time  = new Date() .toLocaleTimeString(); // .toLocaleDateString= Get current time in hh:mm:ss format
+    res.status(200) .send(time )
+})
+
+// how to make index.js to open the browser -whenever running npm run dev:(ask e ma zobtet me3e)
+// app.listen(port, () => {
+//     console.log(`Server running at http://localhost:${port}`);
+//     open(`http://localhost:${port}`);  // This will open the default browser automatically
+//   })
